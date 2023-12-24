ba16e2c0d84bdab7ebd3422bc9ed93b3f6099215
alifertah <alifertah42@gmail.com>
2023-12-24 15:48:50 +0100

    feat : creating login form and init registere

--- views/_layout.php
+++ views/_layout.php
@@ -6,6 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= ucfirst($page) ?></title>
+    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="<?= PATH ?>assets/css/style.css">
 </head>
 <body>
