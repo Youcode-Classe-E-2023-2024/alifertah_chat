f691e15a823e2975a0924cae0b0f1fff4d87af6d
alifertah <alifertah42@gmail.com>
2023-12-26 22:42:21 +0100

    fix : prevent showing all rooms to all users

--- _classes/Room.php
+++ _classes/Room.php
@@ -2,13 +2,14 @@
 
 class Room{
     public $name;
-
     static function getAllRooms(){
+        $usr = $_SESSION['id'];
         global $db;
         return $db->query("SELECT ru.room_id, r.room_name, u.users_id, u.users_username
         FROM room_user ru
         JOIN rooms r ON ru.room_id = r.room_id
-        JOIN users u ON ru.user_id = u.users_id;
+        JOIN users u ON ru.user_id = u.users_id
+        WHERE u.users_id = $usr;
         ");
     }
 }
