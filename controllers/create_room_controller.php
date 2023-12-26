def41d9dbeb64847dd04ce46ff7706ec119ec69c
alifertah <alifertah42@gmail.com>
2023-12-26 22:35:40 +0100

    feat : assingne the room to its owner automatically after creating

--- controllers/create_room_controller.php
+++ controllers/create_room_controller.php
@@ -1,6 +1,10 @@
 <?php 
 if(isset($_POST['submit'])){
     $us = $_SESSION['username'];
+    $usId = $_SESSION['id'];
     global $db;
     $db->query("INSERT INTO rooms (room_name, creator) VALUES ('$_POST[name]', '$us')");
+    $newRoomId = mysqli_insert_id($db);
+    echo($newRoomId . $usId);
+    $db->query("INSERT INTO room_user (room_id, user_id) VALUES ('$newRoomId', '$usId')");
 }
