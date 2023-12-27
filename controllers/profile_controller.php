2bff5419887dfaf768561d02318d4605ad640240
alifertah <alifertah42@gmail.com>
2023-12-27 14:25:04 +0100

    feat : email shown in profile git push

--- controllers/profile_controller.php
+++ controllers/profile_controller.php
@@ -1,2 +1,4 @@
 <?php
     $username = $_GET['username'];
+    $res = $db->query("SELECT users_email FROM users WHERE users_username= '$username'");
+    $userEmail = $res->fetch_assoc();
--- views/profile_view.php
+++ views/profile_view.php
@@ -3,7 +3,7 @@
   <div class="max-w-lg mx-auto my-10 bg-white rounded-lg shadow-md p-5">
     <img class="w-32 h-32 rounded-full mx-auto" src="https://picsum.photos/200" alt="Profile picture">
     <h2 class="text-center text-2xl font-semibold mt-3"><?= $username ?></h2>
-    <p class="text-center text-gray-600 mt-1">Software Engineer</p>
+    <a class="text-center text-gray-600 mt-1"><?=$userEmail['users_email']?></p>
     <div class="flex justify-center mt-5">
       <a href="#" class="text-blue-500 hover:text-blue-700 mx-3">Twitter</a>
       <a href="#" class="text-blue-500 hover:text-blue-700 mx-3">LinkedIn</a>
